a2208e09d57472698a70e2f1e78c25ebb5a4b61d
ebisLab <ebaiden@techie.com>
2019-09-13 02:08:16 -0400

    Post endpoint created

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -22,5 +22,23 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const { VIN, make, model, mileage } = req.body;
+    Cars.insert({ VIN, make, model, mileage })
+        .then(car => {
+            //checking if book exists
+            if (car) {
+                res.status(200).json(car);
+            } else {
+                res.status(400).end({ message: "Sorry there's an error retrieving the data" })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Sorry, error inserting car information" })
+        })
+
+})
+
 
 module.exports = router;
